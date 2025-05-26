4dcd90e09cf92a1b29faa29578858152f59584e9
RosieWitkowski <142828531+RosieWitkowski@users.noreply.github.com>
2025-05-26 17:24:31 +0000

    Custom formatting testing.

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -10,3 +10,6 @@ greet_format = "Hello {}"
 formatted = greet_format.format(name)
 
 print(intrupution, formatted)
+
+personalized_format = "Hi, {}! This is {} speaking. Welcome to {}!"
+print(personalized_format.format(name, "Who?", "the codespace").replace('Who?', 'Rosie'))
