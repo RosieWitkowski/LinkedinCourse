40bd01feb8df3b3cc7dc8b4849d072979286e10c
RosieWitkowski <142828531+RosieWitkowski@users.noreply.github.com>
2025-05-28 20:11:30 +0000

    Dictionary loop examples added.

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -12,6 +12,17 @@ for name in NAMES:
 for name, age in zip(NAMES, AGES):
     print(f"{name} {age}")
 
+# Added dict ver example
+print("DICT EXAMPLE VER 1")
+dict = {"John": 20, "Paul": 21, "George": 22, "Ringo": 23}
+for name in dict:
+    print(name, dict[name])
+    
+print("DICT EXAMPLE VER 2")
+for name, age in dict.items():
+    print(name, age)
+
+
 for name in reversed(NAMES):
     print(name)
 
